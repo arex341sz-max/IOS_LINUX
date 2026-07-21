62d0f1a5b7f7c7e71a39520770e8233d5120ed8f
arex341sz-max <arex341sz@gmail.com>
2026-07-21 23:31:42 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    open: true
+    host: "0.0.0.0",
+    port: 8080,
+    strictPort: true,
+    open: false
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+    strictPort: true
   }
 });
