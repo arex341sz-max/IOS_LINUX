a00ff5bf246a25ae19b53edf0e02514a34bd2b9e
arex341sz-max <arex341sz@gmail.com>
2026-07-21 22:13:23 +0330

    Create MenuBar.jsx

---
+++ components/MenuBar/MenuBar.jsx
@@ -0,0 +1,56 @@
+import "./menu.scss";
+
+
+export default function MenuBar(){
+
+return(
+
+<div className="menu-bar">
+
+<div className="left">
+
+<span className="apple">
+
+</span>
+
+<span>
+Finder
+</span>
+
+<span>
+File
+</span>
+
+<span>
+Edit
+</span>
+
+<span>
+View
+</span>
+
+</div>
+
+
+<div className="right">
+
+<span>
+WiFi
+</span>
+
+<span>
+🔋
+</span>
+
+<span>
+21:30
+</span>
+
+</div>
+
+
+</div>
+
+)
+
+}
